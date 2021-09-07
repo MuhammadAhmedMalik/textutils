52292024b57886e1e6888f10b2eca4dc628400af
Muhammad Ahmed <muhammadahmed7862468@gmail.com>
2021-09-08 02:13:41 +0500

    Wrap TextUtils App

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -36,7 +36,7 @@ export default function Textform(props) {
         </div>
         <div className="container my-3">
             <h2>Text Summary</h2>
-            <p>{text.split(" ").filter((element)=>{return element.length !== 0 }).length} Words and {text.length} Characters</p>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length !== 0 }).length} Words and {text.length} Characters</p>
             <p>{0.008 * text.split(" ").filter((element)=>{return element.length !== 0 }).length} Minutes Read</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:"Nothing to Preview"}</p>
